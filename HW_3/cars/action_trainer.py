1c680b0be45209de3c41ca5b1da68129b9d43e70
Ivan Kireev <ivkireev@yandex.ru>
2018-03-16 23:54:28 +0300

    Учим сеть для второго задания

--- HW_3/cars/action_trainer.py
+++ HW_3/cars/action_trainer.py
@@ -8,6 +8,9 @@ class ActionTrainer(object):
         self._eval_mode = True
         self.RANDOM_ACTION_P = 0.05
 
+        self.bad_choice_p = 0.01
+        self._bad_choice_counter = 0
+
         self.random_default = 0.0
         self.random_bad = 0.8
         self.random_good = 0.1
@@ -15,7 +18,9 @@ class ActionTrainer(object):
 
         self.range = (0.0, 0.0,)
 
-    def set_params(self, low, high, random_default, random_bad, random_good, random_diff):
+        self.steps = [0, 0, 0, 0]
+
+    def set_params(self, low, high, random_default, random_bad, random_good, random_diff, bad_choice_p):
         self.random_default = random_default
         self.random_bad = random_bad
         self.random_good = random_good
@@ -23,34 +28,53 @@ class ActionTrainer(object):
 
         self.range = (low, high,)
 
+        self.bad_choice_p = bad_choice_p
+
     def get_ranges(self):
         return self.range
 
     def set_evaluate(self, mode):
         self._eval_mode = mode
 
+    def reset_steps(self):
+        self.steps = [0, 0, 0, 0]
+
     def choose_action(self, options):
+        self.steps[0] += 1
+
         # ищем действие, которое обещает максимальную награду
         _l, _h = self.get_ranges()
         cl = sum((1 if expected_reward <= _l else 0 for expected_reward, act in options))
         ch = sum((1 if expected_reward >= _h else 0 for expected_reward, act in options))
 
+        if self._bad_choice_counter == 0 and random.random() < self.bad_choice_p:
+            self._bad_choice_counter = 5
+
+        if self._bad_choice_counter > 0:
+            best_pos = 0
+            self._bad_choice_counter -= 1
+        else:
+            best_pos = -1
+
         random_p = self.random_default
         # все плохо
         if cl >= 7:
             random_p = self.random_bad
+            self.steps[1] += 1
 
         # все хорошо
         if ch >= 7:
             random_p = self.random_good
+            self.steps[2] += 1
 
         # есть разные варианты
         if cl >= 2 and ch >= 2:
             random_p = self.random_diff
+            self.steps[3] += 1
 
-        best_action = sorted(options, key=lambda x: x[0])[-1][1]
+        best_action = sorted(options, key=lambda x: x[0])[best_pos][1]
         # Иногда выбираем совершенно рандомное действие
-        if (not self._eval_mode) and (random.random() < random_p):
+        if (not self._eval_mode) and (random.random() < random_p) and self._bad_choice_counter == 0:
             best_action = options[np.random.choice(len(options))][1]
 
         return best_action
